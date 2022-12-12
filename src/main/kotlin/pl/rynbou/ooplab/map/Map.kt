fdd04a2a84123bdf2b203ee1e28f2e38d74467a8
VeryLongNicknameSuchWow <d33p4ck@gmail.com>
2022-12-12 17:41:45 +0100

    Animal bahaviour and genome

--- src/main/kotlin/pl/rynbou/ooplab/map/Map.kt
+++ src/main/kotlin/pl/rynbou/ooplab/map/Map.kt
@@ -5,6 +5,7 @@ import pl.rynbou.ooplab.element.MapElementCardinalDirection
 import pl.rynbou.ooplab.element.MapVector2D
 import pl.rynbou.ooplab.element.animal.Animal
 import pl.rynbou.ooplab.element.animal.AnimalBehaviourProvider
+import pl.rynbou.ooplab.element.animal.AnimalMoveGene
 import pl.rynbou.ooplab.element.animal.AnimalMutationProvider
 import pl.rynbou.ooplab.element.plant.PlantGrowthProvider
 
@@ -24,11 +25,14 @@ class Map(
         for (i in 1..simulationProperties.initialAnimals) {
             animalStorage.addAnimal(
                 Animal(
+                    simulationProperties = simulationProperties,
                     position = MapVector2D.randomVectorInBounds(simulationProperties),
                     cardinalDirection = MapElementCardinalDirection.randomDirection(),
                     birthEpoch = currentEpoch,
                     age = 0,
-                    energy = simulationProperties.initialAnimalEnergy
+                    energy = simulationProperties.initialAnimalEnergy,
+                    genome = MutableList(simulationProperties.genomeLength) { AnimalMoveGene.getRandom() },
+                    currentGeneIndex = 0
                 )
             )
         }
