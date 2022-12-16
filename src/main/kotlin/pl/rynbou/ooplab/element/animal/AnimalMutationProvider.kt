3d9a511613df8a8992f4fd2371d5d50c6cb9722e
VeryLongNicknameSuchWow <d33p4ck@gmail.com>
2022-12-16 18:26:05 +0100

    Implement AnimalMutationProvider

--- src/main/kotlin/pl/rynbou/ooplab/element/animal/AnimalMutationProvider.kt
+++ src/main/kotlin/pl/rynbou/ooplab/element/animal/AnimalMutationProvider.kt
@@ -1,24 +1,30 @@
 package pl.rynbou.ooplab.element.animal
 
 import pl.rynbou.ooplab.SimulationProperties
+import kotlin.random.Random
 
 
 sealed class AnimalMutationProvider(
     protected val simulationProperties: SimulationProperties
 ) {
-    abstract fun mutate(animal: Animal)
+    fun mutate(animal: Animal) {
+        val mutationChance = Random.nextDouble()
+        animal.genome.replaceAll { if (Random.nextDouble() <= mutationChance) it else changeGene(it) }
+    }
+
+    protected abstract fun changeGene(animalMoveGene: AnimalMoveGene): AnimalMoveGene
 
     class RandomAnimalMutationProvider(simulationProperties: SimulationProperties) :
         AnimalMutationProvider(simulationProperties) {
-        override fun mutate(animal: Animal) {
-            TODO()
+        override fun changeGene(animalMoveGene: AnimalMoveGene): AnimalMoveGene {
+            return AnimalMoveGene.getRandom()
         }
     }
 
     class SubtleAnimalMutationProvider(simulationProperties: SimulationProperties) :
         AnimalMutationProvider(simulationProperties) {
-        override fun mutate(animal: Animal) {
-            TODO()
+        override fun changeGene(animalMoveGene: AnimalMoveGene): AnimalMoveGene {
+            return if (Random.nextBoolean()) animalMoveGene.inc() else animalMoveGene.dec()
         }
     }
 }
