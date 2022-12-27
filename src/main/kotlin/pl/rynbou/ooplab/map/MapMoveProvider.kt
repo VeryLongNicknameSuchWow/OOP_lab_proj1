5c1228e8da7fde8dc56e01a8a92e6dc7f701a3af
integraledelebesgue <jsmolka@student.agh.edu.pl>
2022-12-27 12:42:23 +0100

    Animal position transformation fix

--- src/main/kotlin/pl/rynbou/ooplab/map/MapMoveProvider.kt
+++ src/main/kotlin/pl/rynbou/ooplab/map/MapMoveProvider.kt
@@ -3,6 +3,7 @@ package pl.rynbou.ooplab.map
 import pl.rynbou.ooplab.SimulationProperties
 import pl.rynbou.ooplab.element.MapVector2D
 import pl.rynbou.ooplab.element.animal.Animal
+import kotlin.math.abs
 import kotlin.random.Random
 
 sealed class MapMoveProvider(simulationProperties: SimulationProperties, worldMap: WorldMap) {
@@ -10,22 +11,22 @@ sealed class MapMoveProvider(simulationProperties: SimulationProperties, worldMa
     val height = simulationProperties.mapHeight
     val width = simulationProperties.mapWidth
 
-    abstract fun transformCoordinates(position: MapVector2D): MapVector2D
+    abstract fun transformCoordinates(position: MapVector2D): MapVector2D?
 
     fun calculateNewPosition(animal: Animal): MapVector2D {
-        return transformCoordinates(animal.position.add(animal.cardinalDirection.moveVector))
+        return transformCoordinates(animal.position.add(animal.cardinalDirection.moveVector)) ?: animal.position
     }
 
     class GlobeMapMoveProvider(simulationProperties: SimulationProperties, worldMap: WorldMap) :
         MapMoveProvider(simulationProperties, worldMap) {
-        override fun transformCoordinates(position: MapVector2D): MapVector2D {
+        override fun transformCoordinates(position: MapVector2D): MapVector2D? {
             if (position.y < 0)
-                return position
+                return null
 
-            if (position.y > height)
-                return position
+            if (position.y >= height)
+                return null
 
-            return MapVector2D(position.x % width, position.y)
+            return MapVector2D((position.x + width) % width, position.y)
         }
     }
 
@@ -38,7 +39,7 @@ sealed class MapMoveProvider(simulationProperties: SimulationProperties, worldMa
             if (position.x in 0 until width && position.y in 0 until height)
                 return position
 
-            return MapVector2D(Random.nextInt() % width, Random.nextInt() % height)
+            return MapVector2D(abs(Random.nextInt()) % width, abs(Random.nextInt()) % height)
         }
 
     }
